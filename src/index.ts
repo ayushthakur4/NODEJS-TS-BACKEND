2d9ab637a81cc5b9fa3f8a891ecba695f53268d5
_ayushthakur4 <thakurayush9550@gmail.com>
2026-08-15 00:01:10 +0530

    --CLOSE-UP

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import express from 'express';
+import type { Request, Response, Express } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors'
 
 dotenv.config();
 
-const app = express();
+
+
+const app: Express = express();
 
 // Safely parse port from env
 const port: number = Number(process.env.PORT) || 8080;
 
- 
+// Basic endpoint
+app.use(cors({
+    origin: process.env.HOST_URL ||"*"
+}));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello Ayush');
+});
 
 app.listen(port, () => {
   console.log(`✅ Server running at http://localhost:${port}`);
 });
+
