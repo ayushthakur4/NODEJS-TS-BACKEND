587cfcb4657427051479beb0fddbe3813dd6a941
_ayushthakur4 <thakurayush9550@gmail.com>
2026-08-15 12:23:40 +0530

    bookroutes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import type { Request, Response, Express } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import { connect } from 'http2';
-import connectDb from './utils/db.js';
+import connectDb from "../utils/db.ts";
+
 
 dotenv.config();
 
